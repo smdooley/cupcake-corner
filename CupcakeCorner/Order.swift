dfc46d60165bf2e2bff6ee5414fcde51e20045d9
Sean Dooley <s.dooley@ctidigital.com>
2024-11-26 11:01:22 +0000

    taking basic order details

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Sean Dooley on 26/11/2024.
+//
+
+import SwiftUI
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    var type = 0
+    var quantity = 3
+
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
